9b33e6ba2aeac9236305be8b6f0851b7ce602db7
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:03:23 -0300

    O que eh npm?
    Teoria

---
+++ Teorias e instalacoes/teoria2.js
@@ -0,0 +1,8 @@
+// O que eh npm? //
+
+//--> O npm eh um gerenciador de pacotes do Node;
+//--> Vamos poder utilizar bibliotecas de terceiros, baixando elas pelo npm;
+//--> E tambem executar determinados scripts no nosso programa;
+//--> Dificilmente um software em Node.js nao utiliza o npm;
+//--> Os modulos externos ficam numa pasta chamada node_modules;
+//--> Ela deve ser descartavel, ou seja, a cada instalacao do projeto baixamos todos os pacotes novamente;
