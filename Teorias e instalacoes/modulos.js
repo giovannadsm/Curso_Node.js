ab4c273993822574162f01eac9f1137bde1eab80
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 06:15:09 -0300

    O que são modulos ?
    Teoria

---
+++ Teorias e instalacoes/modulos.js
@@ -0,0 +1,9 @@
+// O que são módulos ? //
+
+//
+//--> Módulos são scripts reaproveitáveis, que utilizamos bastante programando em Node;
+//--> Eles são divididos em três categorias;
+//--> Internos: módulos que nós desenvolvemos;
+//--> Core Modules: módulos que vem com o Node.js;
+//--> Externos: módulos que instalamos via npm
+//
