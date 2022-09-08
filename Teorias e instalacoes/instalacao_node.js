38239cf922d5d2eced748424ce69c1f10d5a70ae
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:22:49 -0300

    Instalação Node no Windows
    Instalação

---
+++ Teorias e instalacoes/instalacao_node.js
@@ -0,0 +1,7 @@
+// Instalacao Node no Windows //
+
+//--> O download do Node.js eh feito no site oficial: nodejs.org;
+//--> Vamos baixar um arquivo .msi, que eh o instalador;
+//--> Eh interessante saber que o npm vem junto do Node;
+//--> Apos a instalacao podemos testar o node e o npm em um terminal, para validar a instalacao;
+//--> Note que os binarios precisam estar no PATH, este passo esta na instalacao do Node;
