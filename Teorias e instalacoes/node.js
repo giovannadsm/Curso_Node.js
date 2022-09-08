e67c1a3829d6f538bd280ccf8e52087b24c77583
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:12:44 -0300

    O que é Node.js ?
    Teoria

--- Teorias e instalacoes/node.js
+++ Teorias e instalacoes/node.js
@@ -4,4 +4,4 @@
 //--> Ou seja, uma biblioteca utilizada por um compilador durante a execucao do programa;
 //--> Que esta construida na V8 engine (escrita em C++) da Google;
 //--> Possibilitando criar softwares em JS no lado do servidor;
-//--> Temos entao um  codigo JS rodando em C++ para garantir alta performace;
+//--> Temos entao um  codigo JS rodando em C++ para garantir alta performace.
