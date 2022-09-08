a0da9bff8a50ec3475ebd0eef780be2282a89d54
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:25:55 -0300

    Instalação Node no Linux
    Instalação

---
+++ Teorias e instalacoes/instalacao_node_linux.js
@@ -0,0 +1,8 @@
+// Instalacao Node no Linux //
+
+// 
+//--> O download do Node.js eh feito no site oficial: nodejs.org;
+//--> Vamos baixar um executavel que inicializa a instalacao;
+//--> Note que o npm vem junto do Node;
+//--> Apos a instalacao precisamos validar no terminal;
+//
