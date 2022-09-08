c8566ed2c7bd5b3fa79def0d29989b2969665e17
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:31:31 -0300

    Terminal para Windows
    Teoria

---
+++ Teorias e instalacoes/terminal_windows.js
@@ -0,0 +1,9 @@
+// Terminal para Windows //
+
+// 
+//--> Caso seja necessario, vamos utilizar o cmder como terminal no Windows;
+//--> Este software nao quer instalacao;
+//--> E eh muito semelhante ao terminal do Linux;
+//--> Geralmente utilizamos servidores em linux, entao estar acostumado com esse tipo de terminal eh de grande ajuda;
+//--> Alem disso, algumas ferramentas como Node e Docker, por exemplo, utilizam muito o terminal;
+//
