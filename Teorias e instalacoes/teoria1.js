bad51176466f3bb32c47390eed2d6e0cda3bb344
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:00:03 -0300

    O que eh Node.js ?
    Teoria

---
+++ Teorias e instalacoes/teoria1.js
@@ -0,0 +1,7 @@
+// O que eh Node.js ? //
+
+//--> O Node.js eh uma runtime de JavaScript;
+//--> Ou seja, uma biblioteca utilizada por um compilador durante a execucao do programa;
+//--> Que esta construida na V8 engine (escrita em C++) da Google;
+//--> Possibilitando criar softwares em JS no lado do servidor;
+//--> Temos entao um  codigo JS rodando em C++ para garantir alta performace;
