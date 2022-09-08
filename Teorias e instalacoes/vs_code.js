90b94e14be44cf756464dbc75384b9b53d0fbca9
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:27:39 -0300

    VS Code
    Teoria

---
+++ Teorias e instalacoes/vs_code.js
@@ -0,0 +1,8 @@
+// VS Code // 
+
+// 
+//--> O editor utilizado sera o VS code;
+//--> Ele tem algumas vantagens em trabalhar com Node tambem, como o terminal integrado;
+//--> Alem de ser atualizado constantemente, eh mantido pela Microsoft; 
+//--> Muito utilizado no ambiente corporativo;
+//
