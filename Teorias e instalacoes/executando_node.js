7eb53810f6cffe6986d4907290875beb80c1d46b
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-08 04:33:21 -0300

    Executando o Node
    Teoria

---
+++ Teorias e instalacoes/executando_node.js
@@ -0,0 +1,9 @@
+// Executando o Node //
+
+// 
+//--> Na maioria das vezes estaremos executando o Node via arquivos do nosso projeto;
+//--> Porem eh possivel tambem executa-lo via terminal;
+//--> Basta digitar: node;
+//--> O Node.js possui todas as funcionalidades de JavaScript;
+//--> O nome do recurso eh REPL (Read Evaluate Print Loop);
+//
