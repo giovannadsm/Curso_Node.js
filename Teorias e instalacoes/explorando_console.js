9cdff721ddebd9fa355f2bb560bd0dd931b5ae27
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-09 06:37:10 -0300

    Explorando o console
    Teoria

---
+++ Teorias e instalacoes/explorando_console.js
@@ -0,0 +1,7 @@
+// Explorando o console //
+//
+//● Temos uma grande variedade de métodos no console, como o console.log;
+//● Podemos imprimir mais de uma variável por vez;
+//● Inserir variáveis entre strings;
+//● Há um método para limpar as mensagens de console;
+//
