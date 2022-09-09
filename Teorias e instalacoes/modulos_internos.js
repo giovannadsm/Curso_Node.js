866df8788fadff6096de993ed24de78ff49b76db
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-09 02:01:38 -0300

    Módulos internos
    Teoria

---
+++ Teorias e instalacoes/modulos_internos.js
@@ -0,0 +1,9 @@
+// Módulos internos //
+
+//
+//● Os módulos internos são criados nas pastas do nosso projeto;
+//● Precisamos exportar o módulo;
+//● Podemos utilizar a instrução module.exports;
+//● E importar onde precisamos utilizar;
+//● Para importar vamos utilizar a instrução require;
+//
