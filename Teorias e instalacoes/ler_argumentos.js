003ddf5dabd67f2ff544c321eb4a4131c35dff46
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-09 06:29:06 -0300

    Ler argumentos
    Teoria

---
+++ Teorias e instalacoes/ler_argumentos.js
@@ -0,0 +1,7 @@
+// Ler argumentos//
+//
+//● O Node permite o envio de argumentos via linha de comando;
+//● Passamos eles após a instrução de execução do arquivo;
+//● Os argumentos ficam em um array chamado: process.argv
+//● Onde podemos fazer um loop e resgatar os valores enviados;
+//
