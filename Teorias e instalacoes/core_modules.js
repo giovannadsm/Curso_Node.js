c14ac4e9c7fb814d3c4d86eac448b695b99422ee
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-09 06:26:28 -0300

    Core Modules
    Teoria

---
+++ Teorias e instalacoes/core_modules.js
@@ -0,0 +1,7 @@
+// Core Modules //
+
+//
+//● No Node temos diversos Core Modules, que são os que vêm prontos para serem utilizados;
+//● Eles resolvem diversos problemas, como: trabalhar com arquivos e diretórios, servir aplicações e etc.
+//● Precisamos importar estes módulos no projeto para poder utilizar;
+//
