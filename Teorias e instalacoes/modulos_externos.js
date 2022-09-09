f6e53f728c7b3a701f7ecf86a09b6b9523ec4fd9
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-09 06:34:28 -0300

    Módulos externos
    Teoria

---
+++ Teorias e instalacoes/modulos_externos.js
@@ -0,0 +1,8 @@
+// Módulos externos //
+//
+//● Os módulos externos podem ser instalados via npm;
+//● Para isso precisamos inicializar o npm no projeto, com: npm init;
+//● A partir daí os módulos ficam mapeados e podemos instalar módulos;
+//● Que são salvos na pasta node_modules;
+//● Podemos instalar módulos com npm install <nome>;
+//
