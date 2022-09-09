513422e5e5b651e02aed52c6877185d545de6ef9
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-09 06:39:39 -0300

    Melhorando a visualização
    Teoria

---
+++ Teorias e instalacoes/melhorando_visualizacao.js
@@ -0,0 +1,6 @@
+// Melhorando a visualização //
+//
+//● Há um módulo externo chamado chalk;
+//● Ele pode deixar a visualização do console mais agradável;
+//● Fazendo com que seja possível expressar um feedback com base em cores;
+//
