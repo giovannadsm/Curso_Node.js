046c06dfbcaf60028b5a048033027059ea3c8fef
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-10 02:05:40 -0300

    Síncrono e Assíncrono \  sync e async
    Teoria

---
+++ Teorias e instalacoes/sincrono_ assincrono.js
@@ -0,0 +1,8 @@
+// Síncrono e Assíncrono //
+
+//
+//● Em Node.js temos duas opções ao executar métodos;
+//● Conhecidas como sync e async;
+//● Forma síncrona: o código espera ser totalmente executado para prosseguir;
+//● Forma assíncrona: o código continue progredindo e em um ponto futuro obtém a resposta da execução assíncrona;
+//
