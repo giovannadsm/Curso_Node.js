3a2f3529f33a195b916fc7e04da19c9a6609878b
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-10 01:56:44 -0300

    Event Emitter
    Teoria

---
+++ Teorias e instalacoes/event_emitter.js
@@ -0,0 +1,9 @@
+// Event Emitter //
+
+//
+//● O Event Emitter se comporta como os eventos do navegador;
+//● Podemos ativar um código em alguns pontos da aplicação, como o início;
+//● É um Core Module chamado events;
+//● Precisamos instanciar a classe EventEmitter que vem deste módulo;
+//● E então utilizar os métodos para atingir nosso objetivo;
+//
