2dbd7e50fd63f6f014103fa2af87f18d58493524
Giovanna Messias <giovannadsmessias@outlook.com>
2022-09-10 01:55:09 -0300

    Event Loop
    Teoria

---
+++ Teorias e instalacoes/event_loop.js
@@ -0,0 +1,7 @@
+// Event Loop //
+//
+//● O Event Loop é um recurso da arquitetura do Node;
+//● O Node.js executa uma linha por vez, e de cima para baixo do código escrito;
+//● Isso nos ajuda a evitar problemas de concorrência, garantindo a execução do código;
+//● Precisamos apenas cuidar com bloqueios no fluxo, como loops infinitos
+//
